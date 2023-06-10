94e623fece4a223070e2c2ed14d2118975f7af22
Henrique Sebastião <henriquesebastiao.web@gmail.com>
2023-06-09 20:40:43 -0400

    add tailwindcss

---
+++ src/routes/cargo/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { CargoService, OpenAPI } from "../../client";
+  OpenAPI.BASE = 'http://localhost:8000';
+
+  async function fetchData() {
+    const cargo = await CargoService.getAllCargosCargoGet();
+    console.log(cargo);
+    console.log(cargo[0]);
+    console.log(cargo[0].id);
+  }
+fetchData();
+</script>
