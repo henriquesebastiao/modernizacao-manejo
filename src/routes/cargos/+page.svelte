2029db7899324335cb847d456f99bd7a92f68381
Ivan Noleto <ivannoleto@hotmail.com>
2023-06-10 15:34:26 -0300

    skeleton

--- src/routes/cargos/+page.svelte
+++ src/routes/cargos/+page.svelte
@@ -7,26 +7,39 @@
 
     OpenAPI.BASE = 'http://localhost:8000';
 
-
     let sourceData = [];
 
     CargoService.getAllCargosCargoGet()
       .then(response => {
           sourceData = response;
-          console.log(sourceData)
+          console.log(sourceData);
       })
       .catch(error => {
           console.error('Erro ao obter os cargos:', error);
       });
 
-    const tableSimple: TableSource = {
+    let tableSimple: TableSource = {
         head: ['ID', 'Nome'],
-        body: tableMapperValues(sourceData, ['id', 'nome']),
-        meta: tableMapperValues(sourceData, ['nome', 'id']),
+        body: [],
+        meta: [],
         foot: ['Total', '<code class="code">5</code>']
     };
 
+    $: {
+        tableSimple.body = tableMapperValues(sourceData, ['id', 'nome']);
+        tableSimple.meta = tableMapperValues(sourceData, ['nome', 'id']);
+    }
+
+    onMount(() => {
+        tableSimple.body = tableMapperValues(sourceData, ['id', 'nome']);
+        tableSimple.meta = tableMapperValues(sourceData, ['nome', 'id']);
+    });
 </script>
+
 <main class=".table-container">
-    <Table source={tableSimple}/>
+    {#if sourceData.length > 0}
+        <Table source={tableSimple}/>
+    {:else}
+        <p>Aguardando dados...</p>
+    {/if}
 </main>
