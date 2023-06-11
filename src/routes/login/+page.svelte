73ac9a57e0b298b995877cc0b755b134c95eec7a
Henrique Sebastião <henriquesebastiao.web@gmail.com>
2023-06-11 09:21:58 -0400

    update

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,21 +1,26 @@
 <style>
     input {
-        text-align: center;
         margin-top: 5px;
     }
 
     .espacamento {
         padding-top: 10px;
     }
+
+    .espacamento_icones {
+        padding-right: 5px;
+    }
 </style>
 
 <body class="flex items-center">
-<div class="card p-10 text-center w-[30%] mx-auto">
+<div class="card p-10 w-[30%] mx-auto">
   <div class="espacamento">
+    <i class="espacamento_icones fa-regular fa-envelope"></i>
     <span>Email:</span>
     <input class="input" type="email" placeholder="email"/>
   </div>
   <div class="espacamento">
+    <i class="espacamento_icones fa-solid fa-lock"></i>
     <span>Senha:</span>
     <input class="input" type="password" placeholder="senha"/>
   </div>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,25 +1,31 @@
 <style>
   input {
-      text-align: center;
       margin-top: 5px;
   }
 
   .espacamento {
       padding-top: 10px;
   }
+
+  .espacamento_icones {
+      padding-right: 5px;
+  }
 </style>
 
 <body class="flex items-center">
-  <div class="card p-10 text-center w-[30%] mx-auto">
+  <div class="card p-10 w-[30%] mx-auto">
     <div>
+      <i class="espacamento_icones fa-solid fa-user"></i>
       <span>Nome de usuário:</span>
       <input class="input" type="text" placeholder="username"/>
     </div>
     <div class="espacamento">
+      <i class="espacamento_icones fa-regular fa-envelope"></i>
       <span>Email:</span>
       <input class="input" type="email" placeholder="email"/>
     </div>
     <div class="espacamento">
+      <i class="espacamento_icones fa-solid fa-lock"></i>
       <span>Senha:</span>
       <input class="input" type="password" placeholder="senha"/>
     </div>
