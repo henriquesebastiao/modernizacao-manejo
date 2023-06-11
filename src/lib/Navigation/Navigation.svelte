f1432e2e086abec51cfbe7c071e13861d084132d
Henrique Sebastião <henriquesebastiao.web@gmail.com>
2023-06-11 15:03:04 -0400

    removing style tag

--- src/lib/Navigation/Navigation.svelte
+++ src/lib/Navigation/Navigation.svelte
@@ -11,7 +11,7 @@
 		<a class="btn btn-sm" href="/" on:click={drawerClose}>INÍCIO</a>
 		<a class="btn btn-sm" href="/about" on:click={drawerClose}>SOBRE</a>
 		<a class="btn btn-sm" href="/" on:click={drawerClose}>BLOG</a>
-		<a class="btn btn-sm" href="/" on:click={drawerClose}>PLANOS</a>
+		<a class="btn btn-sm" href="/pricing" on:click={drawerClose}>PLANOS</a>
 		<a class="btn btn-sm" href="/" on:click={drawerClose}>SUPORTE</a>
 	</span>
 </nav>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,33 +1,19 @@
-<style>
-  input {
-      margin-top: 5px;
-  }
-
-  .espacamento {
-      padding-top: 10px;
-  }
-
-  .espacamento_icones {
-      padding-right: 5px;
-  }
-</style>
-
 <body class="flex items-center">
   <div class="card p-10 w-[30%] mx-auto">
     <div>
-      <i class="espacamento_icones fa-solid fa-user"></i>
+      <i class="pt-0.5 fa-solid fa-user"></i>
       <span>Nome de usuário:</span>
-      <input class="input" type="text" placeholder="username"/>
+      <input class="input mt-0.5" type="text" placeholder="username"/>
     </div>
-    <div class="espacamento">
-      <i class="espacamento_icones fa-regular fa-envelope"></i>
+    <div class="pt-2.5">
+      <i class="pt-0.5 fa-regular fa-envelope"></i>
       <span>Email:</span>
-      <input class="input" type="email" placeholder="email"/>
+      <input class="input mt-0.5" type="email" placeholder="email"/>
     </div>
-    <div class="espacamento">
-      <i class="espacamento_icones fa-solid fa-lock"></i>
+    <div class="pt-2.5">
+      <i class="pt-0.5 fa-solid fa-lock"></i>
       <span>Senha:</span>
-      <input class="input" type="password" placeholder="senha"/>
+      <input class="input mt-0.5" type="password" placeholder="senha"/>
     </div>
     <div class="mt-5">
       <button type="button" class="btn variant-ghost">Cadastrar</button>
