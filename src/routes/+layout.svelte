cb471c699727225d3e9373ab6e2cd93a794deb35
Henrique Sebastião <henriquesebastiao.web@gmail.com>
2023-06-10 22:16:58 -0400

    update header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,12 +11,12 @@
     <svelte:fragment slot="header">
         <Header />
     </svelte:fragment>
-    <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
-    <svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
-    <svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+    <svelte:fragment slot="sidebarLeft"><!--Sidebar Left--></svelte:fragment>
+    <svelte:fragment slot="sidebarRight"><!--Sidebar Right--></svelte:fragment>
+    <svelte:fragment slot="pageHeader"><!--Page Header--></svelte:fragment>
     <!-- Router Slot -->
     <slot />
     <!-- ---- / ---- -->
-    <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+    <svelte:fragment slot="pageFooter"><!--Page Footer--></svelte:fragment>
     <svelte:fragment slot="footer"><Footer /></svelte:fragment>
 </AppShell>
