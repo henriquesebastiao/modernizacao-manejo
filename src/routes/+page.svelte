6a6bbac511e3730da2fcad30292f67da81602380
Henrique Sebastião <henriquesebastiao.web@gmail.com>
2023-06-11 22:33:04 -0400

    updating home screen

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import HomeMain from './home-partials/HomeMain.svelte';
+</script>
+
+<div>
+	<div id="main" class="bg-surface-100-800-token main-gradient">
+		<div class="section-container"><HomeMain /></div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.section-container {
+		@apply w-full max-w-7xl mx-auto p-4 py-16 md:py-24;
+	}
+	/* Main Gradient */
+	.main-gradient {
+		background-image: radial-gradient(
+				at 0% 0%,
+				rgba(var(--color-secondary-500) / 0.33) 0px,
+				transparent 50%
+			),
+			radial-gradient(at 98% 1%, rgba(var(--color-error-500) / 0.33) 0px, transparent 50%);
+	}
+</style>
---
+++ src/routes/home-partials/HomeMain.svelte
@@ -0,0 +1,20 @@
+<div class="grid grid-cols-1 xl:grid-cols-2 gap-4 md:gap-10 items-center">
+	<!-- Info -->
+	<div class="flex flex-col items-center xl:items-start text-center xl:text-left space-y-4">
+		<h1 class="!text-5xl md:!text-6xl max-w-[600px]">A tecnologia aliada a pecuária.</h1>
+		<p class="!text-xl max-w-[475px]">
+			Potencialize o poder do seu rebanho com a nossa ferramenta de gestão de gado: simplifique,
+			mensure, supere!
+		</p>
+		<div class="flex gap-4">
+			<a href="/" class="btn variant-filled-primary">
+				<span>Começar</span>
+				<i class="fa-solid fa-arrow-right-long"></i>
+			</a>
+			<a href="/" class="btn variant-soft-primary">Saiba Mais</a>
+		</div>
+	</div>
+
+	<!-- Components -->
+	<div class="hidden md:inline-block w-full max-w-[650px] mx-auto mt-20"></div>
+</div>
