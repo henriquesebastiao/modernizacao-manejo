0494a60f0f6da988a7250641dfbd5f207dcdfd9c
Ivan Noleto <ivannoleto@hotmail.com>
2023-06-11 23:02:39 -0300

    tema

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang='ts'>
+    import '../theme.postcss';
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.postcss';
     import {AppShell} from "@skeletonlabs/skeleton";
