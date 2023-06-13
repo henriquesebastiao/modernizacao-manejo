80bd157066e56daad095961c699f7dc738fd2613
Ivan Noleto <ivannoleto@hotmail.com>
2023-06-13 01:46:06 -0300

    date

--- src/routes/manejo/pesagem/+page.svelte
+++ src/routes/manejo/pesagem/+page.svelte
@@ -6,17 +6,16 @@
 
   let isFocused = true;
 
-  function getDate() {
+  const date = () => {
     const date = new Date();
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = date.getFullYear();
-
-    return `${day}/${month}/${year}`;
+    return `${year}-${month}-${day}`;
   }
 
   let weightAttributes: PesoLogCreateSchema = {
-    data: getDate(),
+    data: date(),
     peso: 0,
     animal_id: 0,
   };
@@ -48,27 +47,27 @@
   <form on:submit={handleSubmit} use:focusTrap={isFocused}>
     <div class=" card p-8">
       <div class="mb-4">
-        <label class="mb-2">
+        <label class="label mb-2">
           <i class="pr-0.5 fa-solid fa-tag"></i>
           <span>Número:</span>
-        </label>
         <input class="input" type="search" bind:value={weightAttributes.animal_id} placeholder="Número do animal">
+        </label>
       </div>
 
       <div class="mb-4">
         <label class="mb-2">
           <i class="pr-0.5 fa-solid fa-weight-hanging"></i>
           <span>Peso:</span>
-        </label>
         <input class="input" type="number" bind:value={weightAttributes.peso} placeholder="Peso do animal">
+        </label>
       </div>
 
       <div class="mb-4">
         <label class="mb-2">
           <i class="pr-0.5 fa-solid fa-calendar-days"></i>
           <span>Data:</span>
-        </label>
         <input class="input" type="date" bind:value={weightAttributes.data} placeholder="Data da pesagem">
+        </label>
       </div>
 
       <div class="flex items-center justify-center">
