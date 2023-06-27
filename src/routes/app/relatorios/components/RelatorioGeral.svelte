ccf3f5b07df094164133e8f79b5c6d6d97e20123
Ivan Noleto <ivannoleto@hotmail.com>
2023-06-27 14:47:44 -0300

    test

--- src/routes/app/relatorios/components/RelatorioGeral.svelte
+++ src/routes/app/relatorios/components/RelatorioGeral.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
 	import ChartExample from './Charts/ChartExample.svelte';
+	import { onMount } from 'svelte';
+	import type { AnimalSchema } from '../../../../client';
+	import { AnimalService } from '../../../../client';
+
+	let animals: AnimalSchema[] = [];
+
+	onMount(async () => {
+		animals = await AnimalService.animalGetAll();
+	});
 </script>
 
 <div class="flex flex-col" in:fade>
@@ -16,35 +25,25 @@
 	<table class="card table-auto shadow-2xl mt-10">
 		<thead>
 			<tr>
-				<th class="px-4 py-2 border-gray-400 border">Name</th>
-				<th class="px-4 py-2 border-gray-400 border">Age</th>
-				<th class="px-4 py-2 border-gray-400 border">Email</th>
-				<th class="px-4 py-2 border-gray-400 border">Email</th>
-				<th class="px-4 py-2 border-gray-400 border">Email</th>
+				<th class="px-4 py-2 border-gray-400 border">tag</th>
+				<th class="px-4 py-2 border-gray-400 border">sisbov</th>
+				<th class="px-4 py-2 border-gray-400 border">gender</th>
+				<th class="px-4 py-2 border-gray-400 border">mother_id</th>
+				<th class="px-4 py-2 border-gray-400 border">father_id</th>
+				<th class="px-4 py-2 border-gray-400 border">data</th>
 			</tr>
 		</thead>
 		<tbody>
-			<tr>
-				<td class="border-gray-400 border px-4 py-2 max-w-xs">John Doe</td>
-				<td class="border-gray-400 border px-4 py-2 max-w-xs">32</td>
-				<td class="border-gray-400 border px-4 py-2 max-w-xs">johndoe@example.com</td>
-				<td class="border-gray-400 border px-4 py-2 max-w-xs">johndoe@example.com</td>
-				<td class="border-gray-400 border px-4 py-2 max-w-xs">johndoe@example.com</td>
-			</tr>
-			<tr>
-				<td class="border-gray-400 border px-4 py-2 max-w-xs">Jane Smith</td>
-				<td class="border-gray-400 border px-4 py-2 max-w-xs">28</td>
-				<td class="border-gray-400 border px-4 py-2 max-w-xs">janesmith@example.com</td>
-				<td class="border-gray-400 border px-4 py-2 max-w-xs">janesmith@example.com</td>
-				<td class="border-gray-400 border px-4 py-2 max-w-xs">janesmith@example.com</td>
-			</tr>
-			<tr>
-				<td class="border-gray-400 border px-4 py-2 max-w-xs">Bob Johnson</td>
-				<td class="border-gray-400 border px-4 py-2 max-w-xs">45</td>
-				<td class="border-gray-400 border px-4 py-2 max-w-xs">bobjohnson@example.com</td>
-				<td class="border-gray-400 border px-4 py-2 max-w-xs">bobjohnson@example.com</td>
-				<td class="border-gray-400 border px-4 py-2 max-w-xs">bobjohnson@example.com</td>
-			</tr>
+			{#each animals as animal}
+				<tr>
+					<td class="border-gray-400 border px-4 py-2 max-w-xs">{animal.tag}</td>
+					<td class="border-gray-400 border px-4 py-2 max-w-xs">{animal.sisbov}</td>
+					<td class="border-gray-400 border px-4 py-2 max-w-xs">{animal.gender}</td>
+					<td class="border-gray-400 border px-4 py-2 max-w-xs">{animal.mother_id}</td>
+					<td class="border-gray-400 border px-4 py-2 max-w-xs">{animal.father_id}</td>
+					<td class="border-gray-400 border px-4 py-2 max-w-xs">{animal.birth_date}</td>
+				</tr>
+			{/each}
 		</tbody>
 	</table>
 </div>
