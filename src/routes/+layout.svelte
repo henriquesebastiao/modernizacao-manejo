366d81bd80fbb4aaa38b02423b3273fe0cb437f8
Henrique Sebastião <henriquesebastiao.web@gmail.com>
2023-06-27 12:00:21 -0400

    centering elements on the root route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,44 +2,56 @@
 	import "../theme.postcss";
 	import "@skeletonlabs/skeleton/styles/skeleton.css";
 	import "../app.postcss";
-	import { autoModeWatcher, Toast } from "@skeletonlabs/skeleton";
-	import { OpenAPI } from "../client";
+	import {AppShell, autoModeWatcher, Toast} from "@skeletonlabs/skeleton";
+	import {OpenAPI} from "../client";
 	import Header from "$lib/components/Header/Header.svelte";
-	import { user } from "../store";
-	import { onMount } from "svelte";
+	import {user} from "../store";
+	import {onMount} from "svelte";
 
 	OpenAPI.BASE = 'https://api.henriquesebastiao.com';
 
-	onMount(() => {
-		const logStore: any = localStorage.getItem('isLoggedIn');
+    onMount(() => {
+        const logStore: any = localStorage.getItem('isLoggedIn');
 
-		if (logStore) {
-			$user.isLoggedIn = true;
-		}
-	});
+        if (logStore) {
+            $user.isLoggedIn = true;
+        }
+    });
 </script>
 
 <div class="parent-toast">
-	<Toast position="tr" />
+    <Toast position="tr"/>
 </div>
 
-<svelte:head
-	>{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}
-	<link href="/manifest.webmanifest" rel="manifest" />
-	<title>Modernização manejo</title></svelte:head
->
+<svelte:head>
+    {@html `<script>
+import {autoModeWatcher} from "@skeletonlabs/skeleton";
+${autoModeWatcher.toString()} autoModeWatcher();</script>`}
+    <link href="/manifest.webmanifest" rel="manifest"/>
+    <title>Modernização manejo</title>
+</svelte:head>
 
-<div class="w-full h-full">
-	<div class="w-full h-[75px]">
-		<Header />
-	</div>
-	<div class="w-full dynamic-width">
-		<slot />
-	</div>
-</div>
+<!-- AppShell -->
+<AppShell>
+    <!-- Header -->
+    <svete:fragment slot="header">
+        <Header/>
+    </svete:fragment>
+
+    <!-- Page Content -->
+    <div class="flex items-center justify-center dynamic-width">
+        <slot/>
+    </div>
+
+    <!-- Footer -->
+</AppShell>
 
 <style>
-	.dynamic-width {
-		height: calc(100% - 75px);
-	}
+    .dynamic-width {
+        height: calc(100% - 75px);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
