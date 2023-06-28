685107ef9d9cf10b86a7783b79990d0459a6e4c0
Ivan Noleto <ivannoleto@hotmail.com>
2023-06-28 20:19:14 -0300

    fix relatorio

--- src/routes/app/relatorios/components/RelatorioLote.svelte
+++ src/routes/app/relatorios/components/RelatorioLote.svelte
@@ -3,7 +3,7 @@
 	import ChartExample from './Charts/ChartExample.svelte';
 </script>
 
-<div class="w-full h-full flex items-center justify-center max-lg:items-start">
+<div class="w-full h-full flex items-center justify-center max-lg:items-start" in:fade>
 	<div class="card shadow-2xl p-4 flex flex-col justify-between space-y-10">
 		<div>
 			<h1 class="font-bold text-3xl">Relatório lote</h1>
